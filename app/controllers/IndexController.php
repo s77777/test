6faae4527df4598158df0bcd3717a40118f24042
Sergey V. Levchenko <s77777.list.ru>
2018-04-27 08:51:40 +0300

    test

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -10,30 +10,30 @@ class IndexController extends ControllerBase
         $this->view->setRenderLevel(
             View::LEVEL_LAYOUT
         );
-								$this->taskTitle='Список задач ';
-								$this->tag->setTitle($this->taskTitle);
-								$this->view->setVar('taskTitle',$this->taskTitle);
-								$sql='SELECT t.id,t.name,t.priorityid,p.text as priority,t.statusid,s.text as status,DATE_FORMAT(t.timecreate,"%d-%m-%Y %H:%i") as dt
-																FROM Tasks t 
-																INNER JOIN Status s ON t.statusid=s.id
-																INNER JOIN Priority p ON t.priorityid=p.id';
-								$query=$this->modelsManager->createQuery($sql);
-								$TasksList=$query->execute();
+	$this->taskTitle='Список задач ';
+	$this->tag->setTitle($this->taskTitle);
+	$this->view->setVar('taskTitle',$this->taskTitle);
+	$sql='SELECT t.id,t.name,t.priorityid,p.text as priority,t.statusid,s.text as status,DATE_FORMAT(t.timecreate,"%d-%m-%Y %H:%i") as dt
+                FROM Tasks t 
+		INNER JOIN Status s ON t.statusid=s.id
+		INNER JOIN Priority p ON t.priorityid=p.id';
+	$query=$this->modelsManager->createQuery($sql);
+	$TasksList=$query->execute();
         $this->view->render('index', 'index');
     }
     
     public function getSearchListAction()
     {
         $this->view->disable();
-								$data = $this->request->getJsonRawBody();        
+	$data = $this->request->getJsonRawBody();        
         if (!empty($data->search)){
-												$sql='SELECT t.id,t.name,p.text as priority,s.text as status,DATE_FORMAT(t.timecreate,"%d-%m-%Y %H:%i") as dt FROM Tasks t 
-																LEFT JOIN TasksTag tt ON t.id=tt.taskid 
-																INNER JOIN Status s ON t.statusid=s.id
-																INNER JOIN Priority p ON t.priorityid=p.id
-																WHERE t.name like :search: or tt.text like :search: GROUP BY t.id';
-												$query=$this->modelsManager->createQuery($sql);
-												$TasksList=$query->execute(['search'=>'%'.$data->search.'%']);
+            $sql='SELECT t.id,t.name,p.text as priority,s.text as status,DATE_FORMAT(t.timecreate,"%d-%m-%Y %H:%i") as dt FROM Tasks t 
+		LEFT JOIN TasksTag tt ON t.id=tt.taskid 
+		INNER JOIN Status s ON t.statusid=s.id
+		INNER JOIN Priority p ON t.priorityid=p.id
+		WHERE t.name like :search: or tt.text like :search: GROUP BY t.id';
+		$query=$this->modelsManager->createQuery($sql);
+            $TasksList=$query->execute(['search'=>'%'.$data->search.'%']);
         } else {
             $TasksList="";
         }
@@ -41,4 +41,3 @@ class IndexController extends ControllerBase
     }
     
 }
-
